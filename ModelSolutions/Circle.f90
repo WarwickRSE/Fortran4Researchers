2e5fedc31c30bbcea05fb1e363ac4111d7a8d912
Heather Ratcliffe <hratcliffe.glasgow@gmail.com>
2019-12-04 17:04:52 +0000

    Add circle area program

---
+++ ModelSolutions/Circle.f90
@@ -0,0 +1,15 @@
+
+PROGRAM main
+
+  IMPLICIT NONE
+
+  REAL :: radius, area
+
+
+  PRINT*, "Enter radius of circle and press Enter"
+
+  READ(*, *) radius
+
+  PRINT "(A, F8.3, A)", "The area is ", 3.14159*radius**2, " square units"
+
+END PROGRAM
